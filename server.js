f12d6faf6f6a28edefafea9694d48c7f2423a9cb
hectordeluna <hectordelunap@gmail.com>
2019-12-18 17:51:38 -0600

    fix

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
 app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, '/client/public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
 })
 
 app.get('/api/photo/:name', async (req, res) => {
