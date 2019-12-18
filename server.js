5ed1a7a6c570ef0d449e11f0a1cc044607d9ee44
hectordeluna <hectordelunap@gmail.com>
2019-12-18 17:55:54 -0600

    fix

--- server.js
+++ server.js
@@ -4,18 +4,31 @@ const Axios = require('axios');
 var pixels = require('image-pixels');
 var getPalette = require('get-rgba-palette');
 var path = require('path');
+cors = require ('cors'),
 
 const app = express();
 
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
+app.use(function (req, res, next) {
 
-app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
-})
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    // Pass to next layer of middleware
+    next();
+    
+});
 
 app.get('/api/photo/:name', async (req, res) => {
     var photos = [];
@@ -94,8 +107,18 @@ app.get('/api/photo/:name', async (req, res) => {
 
 });
 
-const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  
+  }
+  
 
-app.listen(port, () => {
-    console.log('App listening on port 5000!');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
 });
